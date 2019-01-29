90fea6b3fe7af49c2a59b36609ad7eb21fb88a46
João Eudes Lima <joaoeudes7@gmail.com>
2019-01-29 10:31:01 -0300

    v1.0.2 - fix Bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import Vue from 'vue';
 
 import VEmojiPicker from './components/VEmojiPicker.vue';
+import Categories from './components/Categories.vue';
+import Emojis from './components/Emojis.vue';
+import Helper from './components/Helper.vue';
+import YSvg from './components/VSvg.vue';
 
 export const Components = {
   VEmojiPicker,
+  Categories,
+  Emojis,
+  Helper,
+  YSvg
 }
 
 Object.keys(Components).forEach(name => {
