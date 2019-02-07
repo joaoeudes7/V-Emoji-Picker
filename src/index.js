37bb487eeb3a43026929324483afe075632e90f1
João Eudes Lima <joaoeudes7@gmail.com>
2019-02-07 09:27:10 -0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-// import Vue from "vue";
 import VEmojiPicker from './components/VEmojiPicker/VEmojiPicker.vue';
 
-// const Components = {
-//   VEmojiPicker
-// }
+// Declare install function executed by Vue.use()
+export function install(Vue) {
+	if (install.installed) return;
+	install.installed = true;
+	Vue.component('VEmojiPicker', VEmojiPicker);
+}
 
-// Object.keys(Components).forEach(name => {
-//   Vue.use(name, Components[name])
-// })
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
 
 export default VEmojiPicker;
