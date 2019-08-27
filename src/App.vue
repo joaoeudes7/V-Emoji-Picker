fd1f298afaf0258c6c6861ab99682b0ac5b8c0bd
João Eudes Lima <joaoeudes7@gmail.com>
2019-08-27 00:51:29 -0300

    Added news fonts per default

--- src/App.vue
+++ src/App.vue
@@ -30,3 +30,21 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+@font-face {
+  font-family: Twemoji;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://ucarecdn.com/73cba3b1-b2fa-45d9-89e3-886542974668/)
+    format("truetype");
+}
+
+/* @font-face {
+  font-family: NotomojiColor;
+  font-weight: 400;
+  font-display: swap;
+  src: url(https://cdn.glitch.com/61908de1-dd0a-4359-a54b-6cb6d41bb5fd%2FNotoColorEmoji.ttf?1513108808150)
+    format("truetype");
+} */
+</style>
