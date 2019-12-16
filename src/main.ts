0620f22b465caee6eceaff42d8630999831b1901
João Eudes Lima <joaoeudes7@gmail.com>
2019-12-16 15:58:01 -0300

    Removed App file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
+import VEmojiPicker from "./VEmojiPicker.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: h => h(VEmojiPicker)
 }).$mount("#app");
