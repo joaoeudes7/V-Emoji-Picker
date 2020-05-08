ba81488d27d99eaa600ded71a9f219c649ad0161
João Eudes Lima <joaoeudes7@gmail.com>
2020-05-08 01:30:30 -0300

    Update files of locale

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,50 +1,26 @@
-import Vue from 'vue';
-import defaultLang from './lang/pt-Br';
+let lang = {};
 
-// https://github.com/ElemeFE/element/blob/dev/src/locale/index.js
-
-let lang = defaultLang
-
-let i18nHandle = () => {
-  // const vuei18n = Object.getPrototypeOf(this || Vue).$t;
-
-  // if (typeof vuei18n === 'function' && !!Vue.locale) {
-  //   if (!merged) {
-  //     merged = true;
-  //     Vue.locale(
-  //       Vue.config.lang,
-  //       deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
-  //     );
-  //   }
-  //   return vuei18n.apply(this, arguments);
-  // }
+export const i18n = (translations: Object) => {
+  lang = { ...lang, ...translations }
 }
 
-const use = (initials: Object) => {
-  import(`./lang/${initials}`).then(newLang => {
-    lang = newLang
+export const use = (initials: string) => {
+  if (initials.trim().length) {
+    const it = require(`./lang/${initials}`)['default']
+    i18n(it)
+  }
+}
+
+// Sample parse translation
+export const st = (term: string) => {
+  const properties = term.split(".")
+  let text = lang
+
+  properties.forEach(p => {
+    text = text[p]
   })
+
+  return text
 }
 
-const i18n = (fn) => {
-  i18nHandle = fn || i18nHandle
-}
-
-const t = function (path, options) {
-  // let value = i18nHandle.apply(this, arguments);
-  // if (value !== null && value !== undefined) return value;
-
-  // const array = path.split('.');
-  // let current = lang;
-
-  // for (let i = 0, j = array.length; i < j; i++) {
-  //   const property = array[i];
-  //   value = current[property];
-  //   if (i === j - 1) return format(value, options);
-  //   if (!value) return '';
-  //   current = value;
-  // }
-  return '';
-};
-
-export default { use, i18n, t }
+export default { use, i18n, t: st }
---
+++ src/locale/lang/en-UK.ts
@@ -0,0 +1,14 @@
+export default {
+  search: 'Search ...',
+  categories: {
+    Activity: "Activity",
+    Flags: "Flags",
+    Foods: "Foods",
+    Frequently: "Frequently",
+    Objects: "Objects",
+    Nature: "Nature",
+    Peoples: "Peoples",
+    Symbols: "Symbols",
+    Places: "Places"
+  }
+}
