d6ab7a5f69aa25873494258dfee4111b8150caec
1001v <the1001v@gmail.com>
2020-05-17 23:53:05 +0300

    Russian locale added

---
+++ src/locale/lang/ruRU.ts
@@ -0,0 +1,14 @@
+export default {
+  search: 'Поиск ...',
+  categories: {
+    Activity: "Активность",
+    Flags: "Флаги",
+    Foods: "Еда",
+    Frequently: "Недавние",
+    Objects: "Объекты",
+    Nature: "Природа",
+    Peoples: "Люди",
+    Symbols: "Символы",
+    Places: "Места"
+  }
+}
