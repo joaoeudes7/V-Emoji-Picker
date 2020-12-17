c31d7d028b65537a1a29bcb6b40851858280de7c
Kyryl Bogach <kyryl@myworkout.no>
2020-12-17 16:53:11 +0100

    Add esES locale

---
+++ src/locale/lang/esES.ts
@@ -0,0 +1,14 @@
+export default {
+  search: 'Buscar ...',
+  categories: {
+    Activity: "Actividades",
+    Flags: "Banderas",
+    Foods: "Comidas",
+    Frequently: "Frecuentes",
+    Objects: "Objetos",
+    Nature: "Naturaleza",
+    Peoples: "Personas",
+    Symbols: "Símbolos",
+    Places: "Lugares"
+  }
+};
