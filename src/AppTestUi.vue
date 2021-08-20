b40fc47a3bf8356fc00a2a8cbba7ee70253f0cb9
bug4j <wutos>
2021-08-20 19:15:26 +0800

    opt: use vue3 composition api

--- src/AppTestUi.vue
+++ src/AppTestUi.vue
@@ -1,41 +1,56 @@
 <template>
   <div>
-    <div>
-      <VEmojiPicker :customEmojis="emojis" :continuousList="true" v-if="visible" :dark="true" />
+    <div style="width:max-content">
+      <textarea name="" id="" :value="message"/>
+      <VEmojiPicker :customEmojis="emojis" :continuousList="true" v-if="visible"
+        :limitFrequently="3" :emojisByRow="6" @select="onEmojiSelected" />
     </div>
     <button @click="changeEmojis">Change</button>
     <button @click="changeVisibleEmojis">Toogle View</button>
+    <div content="😁"></div>
   </div>
 </template>
 
 <script lang='ts'>
-import { Vue } from "vue-property-decorator";
-import { Options } from 'vue-class-component';
 import { VEmojiPicker, emojisDefault } from "./index";
-import { defineComponent } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
+import { IEmoji } from "./models/Emoji";
 
 export default defineComponent({
   name: "AppTestUI",
   components: {
     VEmojiPicker
   },
-  data() {
-    return {
-      visible:false,
-      emojis: emojisDefault
-    };
-  },
-  methods:{
-    changeEmojis() {
-       this.emojis.splice(0, 8);
-       console.log(this.emojis);
-    },
-    changeVisibleEmojis() {
-      this.visible = !this.visible;
+  setup() {
+    let visible = ref(true);
+    let emojis = reactive(emojisDefault);
+    function changeEmojis() {
+      emojis.splice(0, 8);
     }
-  },
+    function changeVisibleEmojis() {
+      visible.value = !visible.value;
+    }
+
+    let message = ref('');
+    function onEmojiSelected(emoji:IEmoji) {
+      message.value += emoji.data;
+    }
+
+    return {
+      visible, emojis, message,
+      changeEmojis, changeVisibleEmojis, onEmojiSelected
+    };
+
+  }
 });
 </script>
 
 <style lang='scss' scoped>
+  textarea {
+    display: block;
+    margin-bottom: 12px;
+    width: 100%;
+    box-sizing: border-box;
+    font-size: 20px;
+  }
 </style>
